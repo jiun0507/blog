5951d18016f7c3e4d1b907e12c973cf3bef65c09
Jiun Kim <jkim2@bowdoin.edu>
2021-04-22 22:45:26 +0900

    financialstatement table

--- core/views.py
+++ core/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
+from financial_statement.models import FinancialStatement
 
 
 def home_view(request):
-    return render(request, "home.html")
+    financial_statements = FinancialStatement.objects.all().order_by("-id")[:10]
+    context = {
+        "financial_statements": financial_statements,
+    }
+
+    return render(request, "home.html", context=context)
