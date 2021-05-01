86aadbc12a386eb85d7f34f6e894c40a798e1bcc
Jiun Kim <jkim2@bowdoin.edu>
2021-05-01 20:32:06 +0900

    longer field for company name

--- company/models.py
+++ company/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class CompanyModel(models.Model):
     ticker = models.CharField(max_length=10)
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length=200)
     market = models.CharField(max_length=10)
     locale = models.CharField(max_length=10)
     active = models.BooleanField()
