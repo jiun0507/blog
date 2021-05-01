8214b970e4854d37e3808f15fb01542a1553a12c
Jiun Kim <jkim2@bowdoin.edu>
2021-05-01 20:20:49 +0900

    company name limit'

--- company/models.py
+++ company/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class CompanyModel(models.Model):
     ticker = models.CharField(max_length=10)
-    name = models.CharField(max_length=10)
+    name = models.CharField(max_length=50)
     market = models.CharField(max_length=10)
     locale = models.CharField(max_length=10)
     active = models.BooleanField()
