770082f01bc96c038110d28b42ec263a847a5a7f
Jiun Kim <jkim2@bowdoin.edu>
2021-05-05 13:51:06 +0900

    connection pool kwargs set to none

--- config/settings.py
+++ config/settings.py
@@ -95,7 +95,7 @@ CACHES = {
         "LOCATION": os.getenv("REDIS_TLS_URL"),
         "OPTIONS": {
             "CLIENT_CLASS": "django_redis.client.DefaultClient",
-            "REDIS_CLIENT_KWARGS": {"ssl": True},
+            "CONNECTION_POOL_KWARGS": {"ssl_cert_reqs": None},
         },
     }
 }
