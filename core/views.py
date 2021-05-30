91d47372efdde384b7133ecee2934888dd348fda
Jiun Kim <jkim2@bowdoin.edu>
2021-05-30 21:09:19 +0900

    home.html

--- core/views.py
+++ core/views.py
@@ -1,16 +1,14 @@
 from django.shortcuts import render
 
-from company.models import CompanyModel
-from financial_statement.models import FinancialStatement
 from notification.models import Notification
-from user.models import User
 from valuation.models import Valuation
-from company.use_case.get_company_profile import CompanyProfileUseCase
 from post.models import Post
 
 
 def home_view(request):
     posts = Post.objects.all().order_by("-id")[:4]
+    for post in posts:
+        post.content = post.content[:100]
     notifications = Notification.objects.all().order_by("-id")[:10]
     valuations = Valuation.objects.all().order_by("-id")[:10]
     context = {
--- post/views.py
+++ post/views.py
@@ -1,7 +1,16 @@
+from django.http.response import Http404
 from django.shortcuts import render
 from django.views import View
+from post.models import Post
 
 
 class PostView(View):
-    def get(self, request):
-        return
+    def get(self, request, id):
+        try:
+            post = Post.objects.get(id=id)
+        except Post.DoesNotExist:
+            return redirect("404.html")
+        context = {
+            "post": post,
+        }
+        return render(request, "post/post.html", context=context)
