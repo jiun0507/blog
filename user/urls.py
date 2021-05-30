fecb70c5545356b3543ceeb3dbb017fca00bce38
Jiun Kim <jkim2@bowdoin.edu>
2021-05-30 15:45:26 +0900

    delete login signup logout function'

--- user/urls.py
+++ user/urls.py
@@ -1,20 +1,10 @@
 from django.urls import path
 from user.views import (
-    LoginView,
-    LogoutView,
-    RelationCreateView,
-    RelationDeleteView,
     UserView,
-    SignUpView,
 )
 
 app_name = "user"
 
 urlpatterns = [
-    path("login/", LoginView.as_view(), name="login"),
-    path("logout/", LogoutView.as_view(), name="logout"),
-    path("profile/<int:id>", UserView.as_view(), name="profile"),
-    path("signup/", SignUpView.as_view(), name="signup"),
-    path("follow/<int:followee_id>", RelationCreateView.as_view(), name="follow"),
-    path("unfollow/<int:followee_id>", RelationDeleteView.as_view(), name="unfollow"),
+    path("profile", UserView.as_view(), name="profile"),
 ]
--- user/views.py
+++ user/views.py
@@ -14,101 +14,15 @@ from valuation.models import Valuation
 from . import forms
 
 
-# Create your views here.
-class LoginView(FormView):
-    template_name = "user/login.html"
-    form_class = forms.LoginForm
-    success_url = reverse_lazy("core:home")
-
-    def form_valid(self, form):
-        username = form.cleaned_data.get("username")
-        password = form.cleaned_data.get("password")
-        user = authenticate(self.request, username=username, password=password)
-        if user is not None:
-            login(self.request, user)
-        return super().form_valid(form)
-
-
-class LogoutView(View):
-    def get(self, request):
-        logout(request)
-        return redirect(reverse("core:home"))
-
-
 class UserView(View):
-    def get(self, request, id):
-        try:
-            user = User.objects.get(id=id)
-        except User.DoesNotExist:
-            raise Http404
+    def get(self, request):
+        user = request.user
         valuations = Valuation.objects.filter(user=user)
         notifications = Notification.objects.filter(user=user)
-        if FollowRelation.objects.filter(follower=request.user, followee=user).exists():
-            following = True
-        else:
-            following = False
-        followers = user.followee
 
         context = {
             "notifications": notifications,
             "valuations": valuations,
             "user": user,
-            "following": following,
-            "followers": followers,
-            "number_of_followers": followers.count(),
         }
         return render(request, "user/user.html", context=context)
-
-
-class SignUpView(FormView):
-
-    template_name = "user/signup.html"
-    form_class = forms.SignUpForm
-    success_url = reverse_lazy("core:home")
-
-    def form_valid(self, form):
-        form.save()
-        username = form.cleaned_data.get("username")
-        password = form.cleaned_data.get("password")
-        user = authenticate(self.request, username=username, password=password)
-        if user is not None:
-            login(self.request, user)
-        return super().form_valid(form)
-
-
-class RelationCreateView(View):
-    def get(self, request, followee_id):
-        try:
-            relation = FollowRelation.objects.get(follower=request.user)
-        except FollowRelation.DoesNotExist:
-            relation = FollowRelation.objects.create(follower=request.user)
-
-        try:
-            if followee_id == request.user.id:
-                raise IntegrityError
-            relation.followee.add(followee_id)
-            relation.save()
-        except IntegrityError:
-            return HttpResponse("Can't subscribe yourself")
-        return redirect(reverse("core:home"))
-
-
-class RelationDeleteView(View):
-    def get(self, request, followee_id):
-        try:
-            relation = FollowRelation.objects.get(follower=request.user)
-        except FollowRelation.DoesNotExist:
-            return HttpResponse("Not following")
-        try:
-            if followee_id == request.user.id:
-                raise IntegrityError
-            relation.followee.remove(followee_id)
-            relation.save()
-        except IntegrityError:
-            return HttpResponse("Can't subscribe yourself")
-        except Exception as err:
-            return HttpResponse(
-                "Unidentified error when deleting a followee in FollowRelation instance."
-            )
-
-        return redirect(reverse("core:home"))
