141f7ade4a6b9753446847ce70310602e3adfd00
Jiun Kim <jkim2@bowdoin.edu>
2021-06-12 11:23:04 +0900

    change views

--- post/urls.py
+++ post/urls.py
@@ -2,12 +2,13 @@ from django.urls import path
 from post.views import (
     LogView,
     PostView,
+    list_post,
 )
 
 app_name = "post"
 
 urlpatterns = [
-    path("<int:id>", PostView.as_view(), name="get"),
-    path("list", PostView.as_view(), name="list"),
+    path("<int:post_id>", PostView.as_view(), name="get"),
+    path("list", list_post, name="list"),
     path("logs", LogView.as_view(), name="logs"),
 ]
--- post/views.py
+++ post/views.py
@@ -1,23 +1,36 @@
-from django.views import View
-from post.models import Log, Post
+from django.http import Http404
 from django.shortcuts import redirect, render
+from django.views import View
+
+from post.models import Log, Post
+
+
+def list_post(request):
+    try:
+        posts = Post.objects.all()
+    except Post.DoesNotExist:
+        return redirect("404.html")
+    context = {
+        "posts": posts,
+    }
+    return render(request, "post/posts.html", context=context)
 
 
 class PostView(View):
-    def get(self, request):
+    def get(self, request, post_id):
         try:
-            posts = Post.objects.filter()
+            post = Post.objects.get(id=post_id)
         except Post.DoesNotExist:
-            return redirect("404.html")
+            raise Http404
         context = {
-            "posts": posts,
+            "post": post,
         }
-        return render(request, "post/posts.html", context=context)
+        return render(request, "post/post.html", context=context)
 
 
 class LogView(View):
     def get(self, request):
-        logs = Log.objects.filter()
+        logs = Log.objects.all()
         context = {
             "logs": logs,
         }
