061f67d4c1d8e2b1b23e6a6f3f9932495cb895c8
Jiun Kim <jkim2@bowdoin.edu>
2021-06-12 09:55:00 +0900

    change allowed host

--- config/settings.py
+++ config/settings.py
@@ -36,7 +36,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 DEBUG = bool(os.environ.get("DEBUG", True))
 # DEBUG = False
 
-ALLOWED_HOSTS = ["tranquil-journey-32319.herokuapp.com", "127.0.0.1"]
+ALLOWED_HOSTS = ["jiun-blog.herokuapp.com", "127.0.0.1"]
 # ALLOWED_HOSTS = "*"
 
 # Application definition
