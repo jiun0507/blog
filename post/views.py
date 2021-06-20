f2201256c33d0d1a655579e0527f7240eb532324
Jiun Kim <jkim2@bowdoin.edu>
2021-06-20 12:08:28 +0900

    add pagination and word limit on the template

--- post/views.py
+++ post/views.py
@@ -1,15 +1,19 @@
 from django.http import Http404
 from django.shortcuts import redirect, render
 from django.views import View
-
+from django.core.paginator import Paginator
 from post.models import Log, Post
 
 
 def list_post(request):
     try:
-        posts = Post.objects.all()
+        all_posts = Post.objects.all().order_by("-created_at")
+        page = int(request.GET.get("p", 1))  # 없으면 1로 지정
+        paginator = Paginator(all_posts, 5)  # 한 페이지 당 몇개 씩 보여줄 지 지정
+        posts = paginator.get_page(page)
     except Post.DoesNotExist:
         return redirect("404.html")
+
     context = {
         "posts": posts,
     }
